85ee7c32ddc2f5330516b213ff49e36550fe4d15
suhyun1101 <98930195+suhyun1101@users.noreply.github.com>
2026-05-08 20:03:50 +0900

    Add anomaly augmentation functions and visual check

---
+++ custom/augmentations/__init__.py
@@ -0,0 +1,11 @@
+from .anomaly_augmentations import (
+    continuous_perturbation,
+    gaussian_pointwise_noise,
+    missing_observation,
+    pointwise_spike,
+    level_shift,
+    variance_burst,
+    input_only_pair,
+    level_shift_input_output_pair,
+    variance_burst_input_output_pair,
+)
